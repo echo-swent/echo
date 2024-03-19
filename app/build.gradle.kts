ec9e3f97d4bf7f3a0a6f18b079461b3bafc6a2a0
octogradiste <colin.berger@outlook.com>
2024-03-19 08:50:35 +0100

    Replaced deprecated project.buildDir

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,14 +108,14 @@ tasks.register("jacocoTestReport", JacocoReport::class) {
         "**/*_Factory.class2",
         "**/*_MembersInjector.class",
     )
-    val debugTree = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
+    val debugTree = fileTree("${project.layout.buildDirectory}/tmp/kotlin-classes/debug") {
         exclude(fileFilter)
     }
     val mainSrc = "${project.projectDir}/src/main/java"
 
     sourceDirectories.setFrom(files(mainSrc))
     classDirectories.setFrom(files(debugTree))
-    executionData.setFrom(fileTree(project.buildDir) {
+    executionData.setFrom(fileTree(project.layout.buildDirectory) {
         include("outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec")
         include("outputs/code_coverage/debugAndroidTest/connected/*/coverage.ec")
     })
