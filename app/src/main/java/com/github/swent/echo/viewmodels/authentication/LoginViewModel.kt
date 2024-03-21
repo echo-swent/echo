1572432f680af505a603b82a08fc08c188fc0559
octogradiste <colin.berger@outlook.com>
2024-03-21 11:36:05 +0100

    Implemented basic login view model

---
+++ app/src/main/java/com/github/swent/echo/viewmodels/authentication/LoginViewModel.kt
@@ -0,0 +1,39 @@
+package com.github.swent.echo.viewmodels.authentication
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.swent.echo.authentication.AuthenticationResult
+import com.github.swent.echo.authentication.AuthenticationService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel for the login screen.
+ *
+ * @param auth The authentication service to use.
+ */
+class LoginViewModel(private val auth: AuthenticationService) : ViewModel() {
+
+    private val _state = MutableStateFlow<AuthenticationState>(AuthenticationState.SignedOut)
+
+    /** The current state of the view model. */
+    val state = _state.asStateFlow()
+
+    /**
+     * Sign in with email and password.
+     *
+     * @param email The email of the user.
+     * @param password The password of the user.
+     */
+    fun login(email: String, password: String) {
+        _state.value = AuthenticationState.SigningIn
+        viewModelScope.launch {
+            when (val result = auth.signIn(email, password)) {
+                is AuthenticationResult.Success -> _state.value = AuthenticationState.SignedIn
+                is AuthenticationResult.Error ->
+                    _state.value = AuthenticationState.Error(result.message)
+            }
+        }
+    }
+}
