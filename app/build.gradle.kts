e4deb58803b16e4cc614812d6ef9bf991deea289
octogradiste <colin.berger@outlook.com>
2024-03-25 14:51:40 +0100

    Add mockk dependency for android tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.mockk.android)
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
