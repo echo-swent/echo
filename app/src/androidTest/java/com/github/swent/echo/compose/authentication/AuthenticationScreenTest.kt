9d87fa1395af9d73df3ab5965d452187c26c5a55
octogradiste <colin.berger@outlook.com>
2024-03-26 11:14:50 +0100

    Create authentication screen, will be used by the login and register screen

---
+++ app/src/androidTest/java/com/github/swent/echo/compose/authentication/AuthenticationScreenTest.kt
@@ -0,0 +1,113 @@
+package com.github.swent.echo.compose.authentication
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.github.swent.echo.viewmodels.authentication.AuthenticationState
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class AuthenticationScreenTest {
+
+    companion object {
+        const val ACTION = "Action Button"
+    }
+
+    @get:Rule val composeTestRule = createComposeRule()
+
+    private var authenticationCount = 0
+    private var email = ""
+    private var password = ""
+
+    private fun onAuthenticate(email: String, password: String) {
+        this.email = email
+        this.password = password
+        authenticationCount++
+    }
+
+    @Before
+    fun setUp() {
+        authenticationCount = 0
+        email = ""
+        password = ""
+    }
+
+    @Test
+    fun shouldHaveLoginButtonAndInputFieldsWhenIsSignedOut() {
+        composeTestRule.setContent {
+            AuthenticationScreen(ACTION, AuthenticationState.SignedOut, this::onAuthenticate)
+        }
+        composeTestRule.onNodeWithText(ACTION).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithTag("email-field").assertExists()
+        composeTestRule.onNodeWithTag("password-field").assertExists()
+        composeTestRule.onNodeWithTag("error-message").assertDoesNotExist()
+    }
+
+    @Test
+    fun shouldCallOnAuthenticateWithCorrectParametersWhenActionButtonIsPressedInSignedOutState() {
+        composeTestRule.setContent {
+            AuthenticationScreen(ACTION, AuthenticationState.SignedOut, this::onAuthenticate)
+        }
+        composeTestRule.onNodeWithTag("email-field").performTextInput("test@test.test")
+        composeTestRule.onNodeWithTag("password-field").performTextInput("password")
+        composeTestRule.onNodeWithTag("action-button").performClick()
+        assertEquals(1, authenticationCount)
+        assertEquals("test@test.test", email)
+        assertEquals("password", password)
+    }
+
+    @Test
+    fun shouldHaveSigningInTextWhenIsSigningIn() {
+        composeTestRule.setContent {
+            AuthenticationScreen(ACTION, AuthenticationState.SigningIn, this::onAuthenticate)
+        }
+        composeTestRule.onNodeWithText("Signing in...").assertExists()
+    }
+
+    @Test
+    fun shouldHaveSignedInTextWhenIsSignedIn() {
+        composeTestRule.setContent {
+            AuthenticationScreen(ACTION, AuthenticationState.SignedIn, this::onAuthenticate)
+        }
+        composeTestRule.onNodeWithText("Signed in").assertExists()
+    }
+
+    @Test
+    fun shouldHaveErrorTextWhenIsError() {
+        val message = "Error message"
+        composeTestRule.setContent {
+            AuthenticationScreen(ACTION, AuthenticationState.Error(message), this::onAuthenticate)
+        }
+        composeTestRule.onNodeWithText(message).assertExists()
+    }
+
+    @Test
+    fun shouldHaveLoginButtonAndInputFieldsWhenIsError() {
+        val message = "Error message"
+        composeTestRule.setContent {
+            AuthenticationScreen(ACTION, AuthenticationState.Error(message), this::onAuthenticate)
+        }
+        composeTestRule.onNodeWithText(ACTION).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithTag("email-field").assertExists()
+        composeTestRule.onNodeWithTag("password-field").assertExists()
+        composeTestRule.onNodeWithTag("error-message").assertExists()
+    }
+
+    @Test
+    fun shouldCallOnAuthenticateWithCorrectParametersWhenActionButtonIsPressedInErrorState() {
+        composeTestRule.setContent {
+            AuthenticationScreen(ACTION, AuthenticationState.Error("Error"), this::onAuthenticate)
+        }
+        composeTestRule.onNodeWithTag("email-field").performTextInput("test@test.test")
+        composeTestRule.onNodeWithTag("password-field").performTextInput("password")
+        composeTestRule.onNodeWithTag("action-button").performClick()
+        assertEquals(1, authenticationCount)
+        assertEquals("test@test.test", email)
+        assertEquals("password", password)
+    }
+}
---
+++ app/src/main/java/com/github/swent/echo/compose/authentication/AuthenticationScreen.kt
@@ -0,0 +1,38 @@
+package com.github.swent.echo.compose.authentication
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.github.swent.echo.viewmodels.authentication.AuthenticationState
+
+/**
+ * A screen for authenticating a user. It displays a form for the user to enter their email and
+ * password. The user can authenticate by clicking the action button.
+ *
+ * The purpose of this screen is to group into a single component what is common to the login and
+ * registration screens.
+ *
+ * @param action The text to be displayed on the action button.
+ * @param state The current state of the authentication process.
+ * @param onAuthenticate The callback to be invoked when the user clicks the action button. It
+ *   receives the user's email and password as parameters.
+ */
+@Composable
+fun AuthenticationScreen(
+    action: String,
+    state: AuthenticationState,
+    onAuthenticate: (email: String, password: String) -> Unit,
+) {
+    Box(modifier = Modifier.padding(24.dp)) {
+        when (state) {
+            is AuthenticationState.SignedOut -> AuthenticationForm(action, onAuthenticate)
+            is AuthenticationState.SigningIn -> Text("Signing in...")
+            is AuthenticationState.SignedIn -> Text("Signed in")
+            is AuthenticationState.Error ->
+                AuthenticationForm(action, onAuthenticate, error = state.message)
+        }
+    }
+}
