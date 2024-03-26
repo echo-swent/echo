ccb70a75a554fca0b826f8d6708e2524a04e67b9
Noah Robert <noah.robert@epfl.ch>
2024-03-26 09:33:42 +0100

    removed unused colors

--- app/src/main/java/com/github/swent/echo/ui/theme/Color.kt
+++ app/src/main/java/com/github/swent/echo/ui/theme/Color.kt
@@ -2,9 +2,6 @@ package com.github.swent.echo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 val DarkGrey = Color(0xFF333333)
 val White = Color(0xFFFFFFFF)
 
