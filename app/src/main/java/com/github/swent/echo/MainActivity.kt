f731745e47a43f58123fe54aee05092c1f17bb6f
Noah Robert <noah.robert@epfl.ch>
2024-03-26 10:43:58 +0100

    removed unused import directives

--- app/src/main/java/com/github/swent/echo/MainActivity.kt
+++ app/src/main/java/com/github/swent/echo/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
