0a976bea432d2745e5a2cee428890bc74f4d4311
octogradiste <colin.berger@outlook.com>
2024-03-26 19:49:00 +0100

    Fixed unnecessary suspend

--- app/src/main/java/com/github/swent/echo/authentication/AuthenticationService.kt
+++ app/src/main/java/com/github/swent/echo/authentication/AuthenticationService.kt
@@ -26,5 +26,5 @@ interface AuthenticationService {
      *
      * @return The current user's ID, or null if the user is not signed in.
      */
-    suspend fun getCurrentUserID(): String?
+    fun getCurrentUserID(): String?
 }
--- app/src/test/java/com/github/swent/echo/fakes/FakeAuthenticationService.kt
+++ app/src/test/java/com/github/swent/echo/fakes/FakeAuthenticationService.kt
@@ -37,7 +37,7 @@ class FakeAuthenticationService : AuthenticationService {
         return signedInResult.await()
     }
 
-    override suspend fun getCurrentUserID(): String? {
+    override fun getCurrentUserID(): String? {
         return userID
     }
 }
