a396c649ff51844f057a043289c3b5a9cbe566b4
octogradiste <colin.berger@outlook.com>
2024-03-26 11:15:09 +0100

    Create login screen

---
+++ app/src/main/java/com/github/swent/echo/compose/authentication/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.github.swent.echo.compose.authentication
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.github.swent.echo.viewmodels.authentication.LoginViewModel
+
+@Composable
+fun LoginScreen(loginViewModel: LoginViewModel) {
+    val state by loginViewModel.state.collectAsState()
+    AuthenticationScreen(action = "Login", state = state, onAuthenticate = loginViewModel::login)
+}
